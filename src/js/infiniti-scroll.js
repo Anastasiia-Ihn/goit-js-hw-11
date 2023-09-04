0e890bd70942dee58a6d6f05c00b986626c31f81
Anastasiia-Ihn <128808242+Anastasiia-Ihn@users.noreply.github.com>
2023-09-04 21:52:08 +0200

    added shadow

--- src/js/infiniti-scroll.js
+++ src/js/infiniti-scroll.js
@@ -12,7 +12,7 @@ const guardEl = document.querySelector('.js-guard');
 
 const perPage = 40;
 let currPage = 1;
-export {perPage}
+export {perPage,currPage}
 let valueInput = '';
 let lightbox = new SimpleLightbox('.gallery a');
 
@@ -23,19 +23,6 @@ const observer = new IntersectionObserver(handlerLoadMore, options);
 
 // слухач подій на запит по слову
 searchForm.addEventListener('submit', handlerClickOnForm);
-
-function handlerClickOnForm(evt) {
-    evt.preventDefault();//відміна перезагру сторінки
-    gallery.innerHTML=''; // зачистка при новому пошуку
-  valueInput = evt.target.elements[0].value; // те що ввів клієнт
-   currPage = 1; // При новому пошуку повертаємося на першу сторінку
-   
-    if ((valueInput === '') || (valueInput === ' ')) {
-    gallery.innerHTML = '';
-    return Notiflix.Notify.warning(`Sorry, you must enter a value.`);
-  }
-    fetchData()    //  запит на API
-}
 
 function fetchData() {
   fetchCards(currPage, valueInput).then((dataResp) => {
@@ -50,10 +37,23 @@ function fetchData() {
   })
     .catch((_) => Notiflix.Notify.warning('Sorry, there are no images matching your search query. Please try again.'))
 }
+function handlerClickOnForm(evt) {
+    evt.preventDefault();//відміна перезагру сторінки
+    gallery.innerHTML=''; // зачистка при новому пошуку
+  valueInput = evt.target.elements[0].value; // те що ввів клієнт
+   currPage = 1; // При новому пошуку повертаємося на першу сторінку
+   
+    if ((valueInput === '') || (valueInput === ' ')) {
+    gallery.innerHTML = '';
+    return Notiflix.Notify.warning(`Sorry, you must enter a value.`);
+  }
+    fetchData()    //  запит на API
+}
+
+
 
 function handlerLoadMore(entries) {
     entries.forEach((entry) => { 
-        console.log(entry);
         if (entry.isIntersecting) {
             currPage += 1;
           
